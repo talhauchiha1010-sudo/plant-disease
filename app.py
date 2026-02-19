793ab9b46a7431386f007ac15f71cde9469b3cfb
talhauchiha1010-sudo <talhauchiha1010@gmail.com>
2026-02-19 16:15:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,5 @@ def predict():
 # 8️⃣ Run Server
 # ----------------------------------------
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
